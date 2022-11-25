2b8b807d66d5d06c0996424a9d6ce3e2fa1ab493
AlexDrem <119021230+AlexDrem@users.noreply.github.com>
2022-11-25 11:14:12 +0700

    Add files via upload
    4.5

---
+++ 4.5/bin/dart_application_8.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_8/dart_application_8.dart' as dart_application_8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_8.calculate()}!');
+}
